5bcd561fff25facf1e61599d9cd5104280b708a7
AlexF43 <97276854+AlexF43@users.noreply.github.com>
2024-09-14 02:22:26 +1000

    Added grade progress bars and changed updated grade calculation functions

---
+++ UniBuddy/View/GradeCellView.swift
@@ -0,0 +1,18 @@
+//
+//  GradeCellView.swift
+//  UniBuddy
+//
+//  Created by Alex Fogg on 14/9/2024.
+//
+
+import SwiftUI
+
+struct GradeCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GradeCellView()
+}
